5d1f0cde35356596fc8b4035e5bdd62eb60de694
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2021-11-23 21:08:47 +0100

    Dates form user

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def hello(request):
+def hello_view(request):
     return HttpResponse("""<!DOCTYPE html><html><head><title>                Hello
             </title>
         </head>
@@ -14,5 +14,25 @@ def hello(request):
     </html>
     """)
 
-def hello2(request):
+def hello2_view(request):
     return render(request, 'hello2.html')
+
+def adam_view(request):
+    return HttpResponse("Witaj, Adam!")
+
+def ewa_view(request):
+    return HttpResponse("Witaj, Ewa!")
+
+def name_view(request, name):
+    from markupsafe import escape
+    print(escape(name))
+    return HttpResponse(f"Witaj, {name}!")
+
+def name_view2(request, name):
+    return render(
+            request,
+            'name.html',
+        context={
+            'name' :name
+        }
+    )
