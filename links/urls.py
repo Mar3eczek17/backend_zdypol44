45555ec770784bfa6a7b629234fa9aadb71387ad
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2021-11-25 19:04:33 +0100

    Links part 2

--- links/urls.py
+++ links/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from links import views
 
 urlpatterns = [
-    path('first/', views.first),
-    path('second/', views.second),
+    path('mowa/', views.first, name="first"),
+    path('abc/', views.second, name='second'),  # nazwy mapowań
 ]
